cc8ff717f69b2499eb74dbf3a143e0cfedd2fc90
jasonjamesmoore <111584948+jasonjamesmoore@users.noreply.github.com>
2025-08-04 22:54:42 -0400

    favicons

--- music/src/pages/_document.tsx
+++ music/src/pages/_document.tsx
@@ -4,6 +4,26 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      {/* Favicons */}
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/JJM-Favicon-16X16.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/JJM-Favicon-32X32.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/JJM-Favicon-180X180.png"
+      />
+      {/*Default*/}
+      <link rel="shortcut icon" href="/JJM-Favicon-32X32.png" />
       <body className="antialiased">
         <Main />
         <NextScript />
